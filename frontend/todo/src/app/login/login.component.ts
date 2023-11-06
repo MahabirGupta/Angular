cfed5aa1035d0b469ff8f74df61100b26f36fac0
MahabirGupta <mahabirg81@gmail.com>
2023-11-06 17:36:37 +0800

    Adding Hardcoded Authentication to Logic Component - ngIf directive

--- frontend/todo/src/app/login/login.component.ts
+++ frontend/todo/src/app/login/login.component.ts
@@ -9,12 +9,26 @@ export class LoginComponent implements OnInit {
   // set default username and password
   username = 'tiger';
   password = ''; //password is empty
+  errorMessage = 'Invalid Credentials';
+  successMessage = 'Login successfully';
+  successfulLogin = false;
+  invalidLogin = false;
   constructor() {}
 
   ngOnInit(): void {}
 
   handleLogin() {
-    console.log(this.username);
-    console.log(this.password);
+    // console.log(this.username);
+    // Not safe to display password
+    // console.log(this.password);
+    if (this.username === 'tiger' && this.password === '') {
+      // console.log('Login successful');
+      this.successfulLogin = true;
+      this.invalidLogin = false;
+    } else {
+      // console.log(this.errorMessage);
+      this.successfulLogin = false;
+      this.invalidLogin = true;
+    }
   }
 }
