3cb8f9d8595d61500716162d7ad4e4d6db94d9c8
MahabirGupta <mahabirg81@gmail.com>
2023-11-07 15:15:20 +0800

    Create a Todo Class

--- frontend/todo/src/app/list-todos/list-todos.component.ts
+++ frontend/todo/src/app/list-todos/list-todos.component.ts
@@ -1,16 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 
+// Create a Todo class
+export class Todo {
+  // create a constructor
+  constructor(
+    // create a parameters/field for the constructor
+    public id: number,
+    public description: string,
+    public targetDate: Date,
+    public todoStatus: boolean
+  ) {}
+}
+
 @Component({
   selector: 'app-list-todos',
   templateUrl: './list-todos.component.html',
   styleUrls: ['./list-todos.component.css'],
 })
 export class ListTodosComponent implements OnInit {
-  // creating a list of todos using arrays
   todos = [
-    { id: 1, description: 'Learn Kotlin' },
-    { id: 2, description: 'Learn Java' },
-    { id: 3, description: 'Learn AWS' },
+    new Todo(1, 'Learning Kotlin', new Date(), false),
+    new Todo(2, 'Learning Java', new Date(), false),
+    new Todo(3, 'Learning AWS', new Date(), false),
+
+    // creating a list of todos using arrays
+    // { id: 1, description: 'Learn Kotlin' },
+    // { id: 2, description: 'Learn Java' },
+    // { id: 3, description: 'Learn AWS' },
   ];
   // // create a todo object
   // todo = {
