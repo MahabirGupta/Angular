97c26b28dfac4cdb73730495c251688368acb89a
MahabirGupta <mahabirg81@gmail.com>
2024-01-04 00:12:11 +0800

    connect the backend to the frontend to display the message from backend to frontend

--- frontend/todo/src/app/service/data/welcome-data.service.ts
+++ frontend/todo/src/app/service/data/welcome-data.service.ts
@@ -1,13 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+// Define a new class
+export class HelloWorldBean {
+  constructor(public message: string) {}
+}
 @Injectable({
   providedIn: 'root',
 })
 export class WelcomeDataService {
   // Create a method that can be use to invoke the Hello World Service
   executeHelloWorldBeanService() {
-    return this.http.get('http://localhost:8080/hello-world-bean');
+    // defining the response structure I am expecting back
+    return this.http.get<HelloWorldBean>(
+      'http://localhost:8080/hello-world-bean'
+    );
   }
 
   // Create a method which will retrieve the data from the backend service
--- frontend/todo/src/app/welcome/welcome.component.ts
+++ frontend/todo/src/app/welcome/welcome.component.ts
@@ -2,7 +2,10 @@ import { Component, OnInit } from '@angular/core';
 // import org.springframework.boot.SpringApplication;
 import { AppComponent } from '../app.component';
 import { ActivatedRoute, Route, Router } from '@angular/router';
-import { WelcomeDataService } from '../service/data/welcome-data.service';
+import {
+  HelloWorldBean,
+  WelcomeDataService,
+} from '../service/data/welcome-data.service';
 
 //@ComponentScan(value='users')
 @Component({
@@ -16,6 +19,7 @@ export class WelcomeComponent implements OnInit {
   // In Java
   // String message = 'Some Welcome message';
   message = 'Some Welcome message';
+  welcomeMessageFromService: string | undefined;
   name = '';
   // path = '';
 
@@ -42,11 +46,24 @@ export class WelcomeComponent implements OnInit {
   getWelcomeMessage() {
     console.log(this.service.executeHelloWorldBeanService());
     // subscribe to the observable
-    this.service.executeHelloWorldBeanService().subscribe();
+    this.service.executeHelloWorldBeanService().subscribe(
+      // what should we do when we get a response back
+      (response) => this.handleSuccessfulResponse(response)
+      // (response) => console.log(response.message)
+    );
 
+    console.log('last line of getWelcomeMessage()');
     // console.log('get welcome message');
   }
+  handleSuccessfulResponse(response: HelloWorldBean) {
+    this.welcomeMessageFromService = response.message;
+    // console.log(response);
+    // console.log(response.message);
+  }
 }
+
+// create a function to handle a successful response
+
 export class Books {}
 
 export class Students {}
